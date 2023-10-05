284d3868fbb98f079f2c1fe14d64bea7342ac23a
Saipavan8790 <127079433+Saipavan8790@users.noreply.github.com>
2023-10-05 11:53:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //your JS code here. If required.
-function loading(){
-	document.body.innerText = "DOM load success"
-}
+// function loading(){
+// 	document.body.innerText = "DOM load success"
+// }
+	  const load = document.getElementByTag("body")
+load.addEventListener("DOMonload",()=>{
+	load.innerText="DOM load success"
+})
